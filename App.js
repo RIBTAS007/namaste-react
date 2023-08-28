448dc1d8ab9cb50cee9154d538ef6358c5a5aa51
Ribtas007 <ribtas007@outlook.com>
2023-08-28 13:29:39 +0530

    added complex nested structure written using react.

--- App.js
+++ App.js
@@ -1,3 +1,18 @@
-const heading = React.createElement("h1", {}, "Hello world from react");
+// Element, attributes, inner html code
+
+// the heading is a react object using create Element
+
+// the {} things are passed as props.
+
+// reactElement(object) => HTMLAllCollection(browser understands)
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading", xyz: "abc" },
+  "Hello world from react"
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// the render method is responsible for putting object inside h1 and rendering it.
+
 root.render(heading);
