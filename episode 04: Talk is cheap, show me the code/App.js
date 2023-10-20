ae90a6cd80cdfa3845868350ff51ae251c2b26ae
Ribtas007 <ribtas007@outlook.com>
2023-10-20 12:10:09 +0530

    added header component

--- episode 04: Talk is cheap, show me the code/App.js
+++ episode 04: Talk is cheap, show me the code/App.js
@@ -1,64 +1,56 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import image1 from "./logo.jpg";
+
+// - The following will be our components:
+//     - header
+//         - logo
+//         - nav Items
+//     - Body
+//         - Search
+//         - restaurant container
+//         - restaurant card
+//             - image
+//             - rating
+//             - price
+//             - delivery time
+//     - footer
+//         - Copyright
+//         - Links
+//         - Address
+//         - Contact
 
 const Header = () => {
-  return (
+  return(
     <div className="header">
       <div className="logo-container">
-        <img className="logo" src={image1} />
+        <img
+        className="logo" 
+        src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"/>
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About us</li>
-          <li>Contact us</li>
-          <li>Cart</li>
+         <li>Home</li>
+         <li>About Us</li>
+         <li>Contact Us</li>
+         <li>Cart</li>
         </ul>
       </div>
     </div>
   );
 };
 
-// const RestaurantCard =({resName,cuisine}) => { }
-// This is called destructuring on the fly
-// const {resName,cuisine} = props;
-
-const RestaurantCard =(props) => {
-  const {resName,cuisine} = props;
-  return(
-    <div className="res-card">
-      <img 
-      className="res-logo"
-      src={image1}/>
-      <h3>{resName}</h3>
-      <h4>{cuisine}</h4>
-      <h4>4.4. stars</h4>
-      <h4> 38 minutes</h4>
-    </div>
-  )
-}
-
-const Body = ()=>{
-  return(
-    <div className="body">
-      <div className="search">Search</div>
-      <div className="res-container">
-        <RestaurantCard resName="Meghana foods" cuisine="Biryani, north Indian, Asian"/>
-        <RestaurantCard resName="KFC" cuisine="Burger, fast foods"/>
-        
-      </div>
-    </div>
-  )
-};
-
 const AppLayout = () => {
-  return <div className="app">
-    <Header/>
-    <Body/>
-  </div>;
+  return(
+    <div className="app">
+          <Header/>
+    </div>
+  );
 };
 
+//creating a root
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+// To render JSX element
 root.render(<AppLayout/>);
+
+
