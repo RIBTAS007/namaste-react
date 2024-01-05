92d5113d4e186e25650a58f652c52c6da8165c42
Ribtas007 <ribtas007@outlook.com>
2024-01-06 01:14:36 +0530

    completed episode 18

--- namaste-youtube/src/components/Head.js
+++ namaste-youtube/src/components/Head.js
@@ -1,40 +1,91 @@
-import React from "react";
-import { useDispatch } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { toggleMenu } from "../utils/appSlice";
+import { YOUTUBE_SEARCH_API } from "../utils/constants";
+import { cacheResults } from "../utils/searchSlice";
 
 const Head = () => {
-
   const dispatch = useDispatch();
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  const searchCache = useSelector((store) => store.search);
 
-  const toggleMenuHandler = () =>{
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (searchCache[searchQuery]) {
+        setSuggestions(searchCache[searchQuery]);
+      } else {
+        getSearchSuggestions();
+      }
+    }, 200);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  const toggleMenuHandler = () => {
     dispatch(toggleMenu());
-  }
+  };
+
+  const getSearchSuggestions = async () => {
+    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+    const json = await data.json();
+    setSuggestions(json[1]);
+    dispatch(
+      cacheResults({
+        [searchQuery]: json[1],
+      })
+    );
+  };
 
   return (
     <div className="grid grid-flow-col p-5 m-2 shadow-lg">
       <div className="flex col-span-1">
         <img
-        onClick={() => toggleMenuHandler()}
-        className="h-8 cursor-pointer"
+          onClick={() => toggleMenuHandler()}
+          className="h-8 cursor-pointer"
           alt="menu"
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1024px-Hamburger_icon.svg.png"
         />
         <a href="/">
-        <img
-        className="h-8 mx-2"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuE54d8krphaVP1AQ1Jd4G1uRqLcA2N81TnA"
-          alt="youtube"
-        />
+          <img
+            className="h-8 mx-2"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuE54d8krphaVP1AQ1Jd4G1uRqLcA2N81TnA"
+            alt="youtube"
+          />
         </a>
-        
       </div>
       <div className="col-span-10 px-10">
-        <input className="w-1/2 border border-gray-500 p-2 rounded-l-full" type="text" />
-        <button className="border border-gray-400 py-2 px-5 rounded-r-full bg-gray-100">Search</button>
+        <div>
+          <input
+            className="px-5 w-1/2 border border-gray-500 p-2 rounded-l-full"
+            type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onFocus={() => setShowSuggestions(true)}
+            onBlur={() => setShowSuggestions(false)}
+          />
+
+          <button className="border border-gray-400 py-2 px-5 rounded-r-full bg-gray-100">
+            🔍
+          </button>
+        </div>
+        {showSuggestions && (
+          <div className="fixed bg-white py-2 px-2 w-[27rem] shadow-lg rounded-lg border border-gray-100">
+            <ul>
+              {suggestions.map((s) => (
+                <li key={s} className="py-2 px-3 shadow-sm hover:bg-gray-200">
+                  🔍 {s}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
       <div className="col-span-1">
         <img
-        className="h-8"
+          className="h-8"
           src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
           alt="user icon"
         />
